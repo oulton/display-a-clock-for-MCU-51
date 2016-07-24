1f6bb83e2bb345cea5f651003fee85da45d10765
Harper Joy <haipengzhu666@126.com>
2016-07-24 18:39:06 +0800

    Update clock.c

--- clock.c
+++ clock.c
@@ -6,12 +6,12 @@ uchar num=0,snum=0,fnum=0,mnum=0;
 sbit dula=P2^6;
 sbit wela=P2^7;
 uchar code table[]={0x3f,0x06,0x5b,0x4f,
-					0x66,0x6d,0x7d,0x07,
-					0x7f,0x6f,0x77,0x7c,
-					0x39,0x5e,0x79,0x71};
+		    0x66,0x6d,0x7d,0x07,
+		    0x7f,0x6f,0x77,0x7c,
+	            0x39,0x5e,0x79,0x71};
 uchar code tablewela[]={0xff,
-						0xfe,0xfd,0xfb,0xf7,
-						0xef,0xdf,0xbf,0x7f};
+			0xfe,0xfd,0xfb,0xf7,
+			0xef,0xdf,0xbf,0x7f};
 void delay(uint);
 void display(uchar ,uchar ,uchar ,uchar ,uchar ,uchar );
 void main()
@@ -29,74 +29,74 @@ void main()
 }
 void display(uchar sshi,uchar sge,uchar fshi,uchar fge,uchar mshi,uchar mge)
 {
-	dula=1;	   //ËÍÈë¶ÎÑ¡
+	dula=1;	   
 	P0=table[sshi];
 	dula=0;
 	P0=0xff; 
-	wela=1;    //ËÍÈëÎ»Ñ¡
+	wela=1;    
 	P0=tablewela[1];
 	wela=0;
 	delay(2);
 
-	dula=1;	   //ËÍÈë¶ÎÑ¡
+	dula=1;	   
 	P0=table[sge];
 	dula=0;
 	P0=0xff; 
-	wela=1;    //ËÍÈëÎ»Ñ¡
+	wela=1;    
 	P0=tablewela[2];
 	wela=0;
 	delay(2);
 
-	dula=1;	   //ËÍÈë¶ÎÑ¡
+	dula=1;	   
 	P0=0x40;
 	dula=0;
 	P0=0xff; 
-	wela=1;    //ËÍÈëÎ»Ñ¡
+	wela=1;    
 	P0=tablewela[3];
 	wela=0;
 	delay(2);
 
-	dula=1;	   //ËÍÈë¶ÎÑ¡
+	dula=1;	   
 	P0=table[fshi];
 	dula=0;
 	P0=0xff; 
-	wela=1;    //ËÍÈëÎ»Ñ¡
+	wela=1;    
 	P0=tablewela[4];
 	wela=0;
 	delay(2);
 
-	dula=1;	   //ËÍÈë¶ÎÑ¡
+	dula=1;	   
 	P0=table[fge];
 	dula=0;
 	P0=0xff; 
-	wela=1;    //ËÍÈëÎ»Ñ¡
+	wela=1;    
 	P0=tablewela[5];
 	wela=0;
 	delay(2);
 
-	dula=1;	   //ËÍÈë¶ÎÑ¡
+	dula=1;	  
 	P0=0x40;
 	dula=0;
 	P0=0xff; 
-	wela=1;    //ËÍÈëÎ»Ñ¡
+	wela=1;    
 	P0=tablewela[6];
 	wela=0;
 	delay(2);
 
-	dula=1;	   //ËÍÈë¶ÎÑ¡
+	dula=1;	   
 	P0=table[mshi];
 	dula=0;
 	P0=0xff; 
-	wela=1;    //ËÍÈëÎ»Ñ¡
+	wela=1;    
 	P0=tablewela[7];
 	wela=0;
 	delay(2);
 
-	dula=1;	   //ËÍÈë¶ÎÑ¡
+	dula=1;	   
 	P0=table[mge];
 	dula=0;
 	P0=0xff; 
-	wela=1;    //ËÍÈëÎ»Ñ¡
+	wela=1;    
 	P0=tablewela[8];
 	wela=0;
 	delay(2);
